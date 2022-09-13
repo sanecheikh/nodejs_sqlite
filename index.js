d474929b19b51052384b01e5f345bf8af17b41c4
sane cheikh oumar <safietou2717@gmail.com>
2022-09-13 16:53:58 +0200

    ajouter univers

--- index.js
+++ index.js
@@ -6,22 +6,26 @@ let db = new sqlite3.Database(dbname,(err)=>{
     console.log("base de donnee initialise dans le fichier :"
     +dbname);
 });
-//db.run('CREATE TABLE IF NOT EXISTS planetes(name varchar(25));' );
+// db.run('CREATE TABLE IF NOT EXISTS planetes(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), size VARCHAR(255),created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL);');
 
-db.run( "INSERT INTO planetes(name) values (?)",["Mars"]);
-db.run( "INSERT INTO planetes(name) values (?)",["pluton"]);
-db.run( "INSERT INTO planetes(name) values (?)",["Terre"]);
-db.run( "INSERT INTO planetes(name) values (?)",["Jupiter"]);
-db.run( "INSERT INTO planetes(name) values (?)",["Mercure"]);
-
-
-
-//db.all("SELECT rowid,* FROM planetes",(err, data)=>{
-    //if(err) throw  err;
-    //console.log(data);
- //} );
-
- db.run("DELETE FROM planetes WHERE 'name'= 'pluton' ",(err, data)=>{
+/* db.run( "INSERT INTO planetes(name,size) values (?,?)",["Mars ","7586454"]);
+db.run( "INSERT INTO planetes(name,size) values (?,?)",["pluton","213255"]);
+db.run( "INSERT INTO planetes(name,size) values(?,?)",["Terre","6352555"]);
+db.run( "INSERT INTO planetes(name,size) values  (?,?)",["Jupiter","955222"]);
+db.run( "INSERT INTO planetes(name,size) values  (?,?)",["Mercure","122565"]);
+  */
+//db.run("drop table planetes");
+/* 
+db.all("SELECT rowid,* FROM planetes",(err, data)=>{
     if(err) throw  err;
     console.log(data);
- } );
+} ); */
+
+db.each("SELECT rowid,* FROM planetes",(err, data)=>{
+    if(err) throw  err;
+    console.log(data);
+} );
+ /* db.run("DELETE FROM planetes WHERE 'name'= 'pluton' ",(err, data)=>{
+    if(err) throw  err;
+    console.log(data);
+ } ); */
